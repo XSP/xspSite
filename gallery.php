7ef33142a42a62c6441ef1f34b926cf0c94c3766
pavan <pavan@graphicana.co.in>
2013-03-03 23:01:25 +0000

    gallery page update. yet to add real content and styling

--- gallery.php
+++ gallery.php
@@ -11,6 +11,9 @@
 <script src="css_js/jquery1.6.1.js" type="text/javascript"></script>
 <script src="css_js/jquery1.2.js" type="text/javascript"></script>
 <script src="css_js/custom.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
+<link rel="stylesheet" href="css_js/prettyPhoto.css" type="text/css" media="screen" title="prettyPhoto main stylesheet" charset="utf-8" />
+<script src="css_js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript">
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-28624781-1']);
@@ -21,6 +24,24 @@
 		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	})();
 </script>
+<script type="text/javascript" charset="utf-8">
+			$(document).ready(function(){
+				$("area[rel^='prettyPhoto']").prettyPhoto();
+				
+				$(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'light_square',slideshow:3000, autoplay_slideshow: true});
+				$(".gallery:gt(0) a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'fast',slideshow:10000, hideflash: true});
+		
+				$("#custom_content a[rel^='prettyPhoto']:first").prettyPhoto({
+					custom_markup: '<div id="map_canvas" style="width:260px; height:265px"></div>',
+					changepicturecallback: function(){ initialize(); }
+				});
+
+				$("#custom_content a[rel^='prettyPhoto']:last").prettyPhoto({
+					custom_markup: '<div id="bsap_1259344" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div><div id="bsap_1237859" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6" style="height:260px"></div><div id="bsap_1251710" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div>',
+					changepicturecallback: function(){ _bsap.exec(); }
+				});
+			});
+			</script>
 </head>
 <body>
 	<div class="bgWrap">
@@ -33,68 +54,13 @@
 		<br>
 		<div class="wrapperBG">
 		<section class="wrapper">
-			<div id="gallery">
-				<div class="item">
-					<div class="title">Title</div>
-					<div class="tmbNl"></div>
-					<div class="discription">This is a discription, which is not in detail but gives a glimpse.</div>
-					<div class="scrnSht"><img src="#" alt="dummy"></div>
-				</div>
-				<div class="item">
-					<div class="title">Title</div>
-					<div class="tmbNl"></div>
-					<div class="discription">This is a discription, which is not in detail but gives a glimpse.</div>
-					<div class="scrnSht"><img src="#" alt="dummy"></div>
-				</div>
-				<div class="item">
-					<div class="title">Title</div>
-					<div class="tmbNl"></div>
-					<div class="discription">This is a discription, which is not in detail but gives a glimpse.</div>
-					<div class="scrnSht"><img src="#" alt="dummy"></div>
-				</div>
-				<div class="item">
-					<div class="title">Title</div>
-					<div class="tmbNl"></div>
-					<div class="discription">This is a discription, which is not in detail but gives a glimpse.</div>
-					<div class="scrnSht"><img src="#" alt="dummy"></div>
-				</div>
-				<div class="item">
-					<div class="title">Title</div>
-					<div class="tmbNl"></div>
-					<div class="discription">This is a discription, which is not in detail but gives a glimpse.</div>
-					<div class="scrnSht"><img src="#" alt="dummy"></div>
-				</div>
-				<div class="item">
-					<div class="title">Title</div>
-					<div class="tmbNl"></div>
-					<div class="discription">This is a discription, which is not in detail but gives a glimpse.</div>
-					<div class="scrnSht"><img src="#" alt="dummy"></div>
-				</div>
-				<div class="item">
-					<div class="title">Title</div>
-					<div class="tmbNl"></div>
-					<div class="discription">This is a discription, which is not in detail but gives a glimpse.</div>
-					<div class="scrnSht"><img src="#" alt="dummy"></div>
-				</div>
-				<div class="item">
-					<div class="title">Title</div>
-					<div class="tmbNl"></div>
-					<div class="discription">This is a discription, which is not in detail but gives a glimpse.</div>
-					<div class="scrnSht"><img src="#" alt="dummy"></div>
-				</div>
-				<div class="item">
-					<div class="title">Title</div>
-					<div class="tmbNl"></div>
-					<div class="discription">This is a discription, which is not in detail but gives a glimpse.</div>
-					<div class="scrnSht"><img src="#" alt="dummy"></div>
-				</div>
-				<div class="item">
-					<div class="title">Title</div>
-					<div class="tmbNl"></div>
-					<div class="discription">This is a discription, which is not in detail but gives a glimpse.</div>
-					<div class="scrnSht"><img src="#" alt="dummy"></div>
-				</div>
-			</div>
+			<ul class="gallery clearfix">
+				<li><a href="images/fullscreen/2.jpg" rel="prettyPhoto[gallery1]"><img src="images/thumbnails/t_2.jpg" width="60" height="60" alt="Nice building" /></a></li>
+				<li><a href="images/fullscreen/3.jpg" rel="prettyPhoto[gallery1]"><img src="images/thumbnails/t_3.jpg" width="60" height="60" alt="Fire!" /></a></li>
+				<li><a href="images/fullscreen/4.jpg" rel="prettyPhoto[gallery1]"><img src="images/thumbnails/t_4.jpg" width="60" height="60" alt="Rock climbing" /></a></li>
+				<li><a href="images/fullscreen/5.jpg" rel="prettyPhoto[gallery1]"><img src="images/thumbnails/t_5.jpg" width="60" height="60" alt="Fly kite, fly!" /></a></li>
+				<li><a href="images/fullscreen/6.jpg" rel="prettyPhoto[gallery1]"><img src="images/thumbnails/t_2.jpg" width="60" height="60" alt="Nice building" /></a></li>
+			</ul>
 		</section>
 		</div>
 	</div>
