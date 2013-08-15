bf862f3ce2a888748bb3a11f734a0c2a5ddbbee2
pavan <pavan@thexsp.com>
2013-08-15 21:06:00 +0530

    in-progress: portfolio

--- portfolio.php
+++ portfolio.php
@@ -1,3 +1,18 @@
 <div class="wrapper">
     <h2>Show off</h2>
+
+    <div class="gallery">
+        <ul class="gallery">
+            <li>
+                <div>
+                    <div class="img">
+                        <img src="images/works/tumb/1.jpg" alt="valehri"/>
+                        <img src="images/works/tumb/2.jpg" alt="abbs"/>
+                        <img src="images/works/tumb/3.jpg" alt="vastrage"/>
+                        <img src="images/works/tumb/4.jpg" alt="iplay social"/>
+                        <img src="images/works/tumb/5.jpg" alt="kids international school"/></div>
+                </div>
+            </li>
+        </ul>
+    </div>
 </div>
