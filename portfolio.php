778dd01ff8c5d682c99f23247457bbf5a8321da7
pavan <pavan@thexsp.com>
2013-08-18 22:15:59 +0530

    in-progress: portfolio

--- portfolio.php
+++ portfolio.php
@@ -2,17 +2,70 @@
     <h2>Show off</h2>
 
     <div class="gallery">
-        <ul class="gallery">
-            <li>
-                <div>
-                    <div class="img">
-                        <img src="images/works/tumb/1.jpg" alt="valehri"/>
-                        <img src="images/works/tumb/2.jpg" alt="abbs"/>
-                        <img src="images/works/tumb/3.jpg" alt="vastrage"/>
-                        <img src="images/works/tumb/4.jpg" alt="iplay social"/>
-                        <img src="images/works/tumb/5.jpg" alt="kids international school"/></div>
-                </div>
-            </li>
-        </ul>
+        <div class="sec">
+            <img src="images/works/tumb/1.jpg" alt="valehri"/>
+            <div class="cont">
+                <h3>Title</h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim augue sit amet diam
+                    placerat, faucibus convallis augue adipiscing. Praesent tempor sem leo, quis accumsan nibh mollis
+                    nec. Pellentesque quis diam ut sem accumsan accumsan sit amet non diam. Nullam lobortis mauris vel
+                    quam porttitor ultrices. Donec euismod rutrum tincidunt. Integer sit amet vestibulum risus, eu
+                    pellentesque mi. Cras fringilla commodo mauris, ac sagittis velit adipiscing quis. Nunc vitae risus
+                    at sem commodo ornare vitae vitae leo. </p>
+            </div>
+            <span class="border"></span>
+        </div>
+        <div class="sec">
+            <img src="images/works/tumb/2.jpg" alt="abbs"/>
+            <div class="cont">
+                <h3>Title</h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim augue sit amet diam
+                    placerat, faucibus convallis augue adipiscing. Praesent tempor sem leo, quis accumsan nibh mollis
+                    nec. Pellentesque quis diam ut sem accumsan accumsan sit amet non diam. Nullam lobortis mauris vel
+                    quam porttitor ultrices. Donec euismod rutrum tincidunt. Integer sit amet vestibulum risus, eu
+                    pellentesque mi. Cras fringilla commodo mauris, ac sagittis velit adipiscing quis. Nunc vitae risus
+                    at sem commodo ornare vitae vitae leo. </p>
+            </div>
+            <span class="border"></span>
+        </div>
+        <div class="sec">
+            <img src="images/works/tumb/3.jpg" alt="vastrage"/>
+            <div class="cont">
+                <h3>Title</h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim augue sit amet diam
+                    placerat, faucibus convallis augue adipiscing. Praesent tempor sem leo, quis accumsan nibh mollis
+                    nec. Pellentesque quis diam ut sem accumsan accumsan sit amet non diam. Nullam lobortis mauris vel
+                    quam porttitor ultrices. Donec euismod rutrum tincidunt. Integer sit amet vestibulum risus, eu
+                    pellentesque mi. Cras fringilla commodo mauris, ac sagittis velit adipiscing quis. Nunc vitae risus
+                    at sem commodo ornare vitae vitae leo. </p>
+            </div>
+            <span class="border"></span>
+        </div>
+        <div class="sec">
+            <img src="images/works/tumb/4.jpg" alt="iplay social"/>
+            <div class="cont">
+                <h3>Title</h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim augue sit amet diam
+                    placerat, faucibus convallis augue adipiscing. Praesent tempor sem leo, quis accumsan nibh mollis
+                    nec. Pellentesque quis diam ut sem accumsan accumsan sit amet non diam. Nullam lobortis mauris vel
+                    quam porttitor ultrices. Donec euismod rutrum tincidunt. Integer sit amet vestibulum risus, eu
+                    pellentesque mi. Cras fringilla commodo mauris, ac sagittis velit adipiscing quis. Nunc vitae risus
+                    at sem commodo ornare vitae vitae leo. </p>
+            </div>
+            <span class="border"></span>
+        </div>
+        <div class="sec">
+            <img src="images/works/tumb/5.jpg" alt="kids international school"/>
+            <div class="cont">
+                <h3>Title</h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim augue sit amet diam
+                    placerat, faucibus convallis augue adipiscing. Praesent tempor sem leo, quis accumsan nibh mollis
+                    nec. Pellentesque quis diam ut sem accumsan accumsan sit amet non diam. Nullam lobortis mauris vel
+                    quam porttitor ultrices. Donec euismod rutrum tincidunt. Integer sit amet vestibulum risus, eu
+                    pellentesque mi. Cras fringilla commodo mauris, ac sagittis velit adipiscing quis. Nunc vitae risus
+                    at sem commodo ornare vitae vitae leo. </p>
+            </div>
+        </div>
     </div>
+</div>
 </div>
