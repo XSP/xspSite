ee01863d0a548069565cb44ad48b575357600357
pavan <pavan@thexsp.com>
2013-10-28 12:13:52 +0530

    completed: solutions

--- solutions.php
+++ solutions.php
@@ -1,84 +1,75 @@
 <div class="wrapper">
     <div class="solutions">
-        <div class="hexa top white above">
-            <!--<div class="shadow"></div>-->
-        </div>
         <div class="container">
             <div class="scroll"></div>
             <h2>Solutions</h2>
 
-            <div class="solution">
-                <p class="main"> Our characteristics and livelihood are simple, clear and fast. We bring into being
-                    intuitive designs and
-                    apps, no matter whether it’s phone or desktop or other. We have been around the block and are ready
+            <p class="main"> Our characteristics and livelihood are simple, clear and fast. We bring into being
+                intuitive designs and
+                apps, no matter whether it’s phone or desktop or other. We have been around the block and are ready
+                to
+                handle whatever you throw at us. </p>
+        </div>
+        <ul class="detail">
+            <li class="commerce">
+                <h4>Ecommerce and Mobile commerce</h4>
+
+                <p>Have a product and want to sell online, you are at the right place. We provide the solutions
+                    for
+                    Ecommerce and Mobile commerce by developing portals which fits in all the devices like
+                    mobile,
+                    tablets and desktops.
+                </p>
+            </li>
+            <li class="web">
+                <h4>Web applications</h4>
+
+                <p>We have a long history of creating complex web applications. There are huge benefits of
+                    having
+                    secure
+                    system online that address your problem and automate more processes.
+                </p>
+            </li>
+            <li class="responsive">
+                <h4>Responsive Web</h4>
+
+                <p>Today we have numerous devices with different screen size and resolution. We build websites
+                    which
+                    can
+                    fit into all screens and resolution with consistent user experience.
+                </p>
+            </li>
+            <li class="platform">
+                <h4>Cross Platform</h4>
+
+                <p>As the increase in number of different devices, Cross Platform plays a major role in dealing
+                    with
+                    all
+                    the devices. We build hybrid application which runs seamlessly in mobile phones, tablets and
+                    desktop’s with consistent user experiences and mobility.</p>
+            </li>
+            <li class="mobile">
+                <h4>Mobile apps</h4>
+
+                <p>Mobile apps are evolving every day and getting extensively popular as they always stay at
+                    your
+                    fingertips. We develop mobile apps with rich user experience. If you have one in mind that
+                    you
+                    want
+                    to create, drop in a line.</p>
+            </li>
+            <li class="cloud">
+                <h4>Cloud</h4>
+
+                <p> We develop and scale apps in cloud, launch new version without downtime. We use numerous
+                    tools
                     to
-                    handle whatever you throw at us. </p>
-
-                <ul class="detail">
-                    <li>
-                        <h4>Ecommerce and Mobile commerce</h4>
-
-                        <p>Have a product and want to sell online, you are at the right place. We provide the solutions
-                            for
-                            Ecommerce and Mobile commerce by developing portals which fits in all the devices like
-                            mobile,
-                            tablets and desktops.
-                        </p>
-                    </li>
-                    <li>
-                        <h4>Web applications</h4>
-
-                        <p>We have a long history of creating complex web applications. There are huge benefits of
-                            having
-                            secure
-                            system online that address your problem and automate more processes.
-                        </p>
-                    </li>
-                    <li>
-                        <h4>Responsive Web</h4>
-
-                        <p>Today we have numerous devices with different screen size and resolution. We build websites
-                            which
-                            can
-                            fit into all screens and resolution with consistent user experience.
-                        </p>
-                    </li>
-                    <li>
-                        <h4>Cross Platform</h4>
-
-                        <p>As the increase in number of different devices, Cross Platform plays a major role in dealing
-                            with
-                            all
-                            the devices. We build hybrid application which runs seamlessly in mobile phones, tablets and
-                            desktop’s with consistent user experiences and mobility.</p>
-                    </li>
-                    <li>
-                        <h4>Mobile apps</h4>
-
-                        <p>Mobile apps are evolving every day and getting extensively popular as they always stay at
-                            your
-                            fingertips. We develop mobile apps with rich user experience. If you have one in mind that
-                            you
-                            want
-                            to create, drop in a line.</p>
-                    </li>
-                    <li>
-                        <h4>Cloud</h4>
-
-                        <p> We develop and scale apps in cloud, launch new version without downtime. We use numerous
-                            tools
-                            to
-                            develop and push the code to the cloud. A build pack is run on each push and prepares your
-                            apps
-                            for
-                            deployment.
-                        </p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="hexa bottom white below">
-            <!--<div class="shadow"></div>-->
-        </div>
+                    develop and push the code to the cloud. A build pack is run on each push and prepares your
+                    apps
+                    for
+                    deployment.
+                </p>
+            </li>
+        </ul>
     </div>
 </div>
